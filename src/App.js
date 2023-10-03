738b984d9c28041074bb23f39d4161c12589fc2f
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-10-02 21:47:01 -0300

    feat: adding student

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import icon from './assets/register.png';
+
 function App() {
+  const baseUrl = 'https://localhost:7046/api/student';
+
+  const [data, setData] = useState([]);
+
+  const [modalIncluir, setModalIncluir] = useState(false);
+
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    name: '',
+    email: '',
+  });
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado,
+      [name]: value,
+    });
+    console.log(alunoSelecionado);
+  };
+
+  const pedidoGet = async () => {
+    await axios
+      .get(`${baseUrl}/get-all`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const pedidoPost = async () => {
+    delete alunoSelecionado.id;
+    await axios
+      .post(`${baseUrl}/create`, alunoSelecionado)
+      .then((response) => {
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    pedidoGet();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="aluno-container">
+      <br />
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <img src={icon} alt="cadastro" />
+        <button
+          className="btn btn-success"
+          onClick={() => abrirFecharModalIncluir()}
         >
-          Learn React
-        </a>
+          Incluir novo Aluno
+        </button>
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((aluno) => (
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.name}</td>
+              <td>{aluno.email}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button>{' '}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome: </label> <br />
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              onChange={handleChange}
+            />
+            <br />
+            <label>Email: </label> <br />
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPost()}>
+            Incluir
+          </button>{' '}
+          <button
+            className="btn btn-danger"
+            onClick={() => abrirFecharModalIncluir()}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
